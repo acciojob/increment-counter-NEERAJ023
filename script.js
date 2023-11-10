83c7578feca3e37311237bd2c659d1d675560300
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-11 01:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let p=document.querySelector("#counter");
+let btn=document.querySelector("#incrementBtn");
+btn.addEventListener("click",onclick);
+ let i=1;
+function onclick(){
+	p.innerText=i;
+	alert(p.innerText);
+	i++;
+}
